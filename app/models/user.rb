51777db6bb88d0316c977b99e1d888fa331d359e
Braulio <brauliopuentes1995@hotmail.es>
2016-12-08 20:26:08 -0300

    corrección validaciones de usuario

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,13 @@ class User < ActiveRecord::Base
 	validates :name, presence: true, length: {minimum: 6}
 	validates :password, confirmation: true, length: {minimum: 6}
 	validates :password_confirmation, presence: true
-	validates :email, confirmation: true
-	validates :email_confirmation, presence: true
+	validates :email, presence: true
+	validate :user_not_registered
+	validates :email, format: {with: /.+@.+\..+/i}
+end
+
+private
+
+def user_not_registered
+	errors.add :email, 'ya está registrado' if User.find_by_email(email)
 end
