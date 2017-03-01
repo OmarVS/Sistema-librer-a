b27febb82a5d9c8190c769eae550c51bda88fd64
Omar Valle Silva <dark_earthy@hotmail.com>
2017-03-01 04:57:20 -0300

    Agregado formato de moneda en precios

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
     direction = (column == sort_column && sort_direction == "asc") ? "desc" : "asc"
     link_to title, {:sort => column, :direction => direction}
   end
+
+  def moneda(number)
+    number_to_currency(number, :unit => "$ ", :separator => ",", :delimiter => ".", :precision => 0)
+  end
 end
