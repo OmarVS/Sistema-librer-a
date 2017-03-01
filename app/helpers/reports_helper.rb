ddfc3634df53026bad27258df8dca2413c674a8c
Omar Valle Silva <dark_earthy@hotmail.com>
2017-03-01 03:41:12 -0300

    Agregado cálculo en Informe de Compras

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,2 +1,12 @@
 module ReportsHelper
+  def calc_purchases(barcode,year,month)
+    @purchases = Purchase.where('extract(year from created_at) = ? and extract(month from created_at) = ?',year,month)
+    sum = 0
+    @purchases.each do |p|
+      if p.product_barcode == barcode
+        sum += p.amount*p.price
+      end
+    end
+    sum
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -19,24 +19,4 @@ class Product < ActiveRecord::Base
 	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "150x150#" }, :default_url => "/images/products/:style/missing.png"
 	validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
-	private
-		def total
-			suma = 0
-			products.each do |product|
-				suma += product.price
-				ene += product.price if product.created_at.to_date.month==1
-				feb += product.price if product.created_at.to_date.month==2
-				mar += product.price if product.created_at.to_date.month==3
-				abr += product.price if product.created_at.to_date.month==4
-				may += product.price if product.created_at.to_date.month==5
-				jun += product.price if product.created_at.to_date.month==6
-				jul += product.price if product.created_at.to_date.month==7
-				ago += product.price if product.created_at.to_date.month==8
-				sep += product.price if product.created_at.to_date.month==9
-				oct += product.price if product.created_at.to_date.month==10
-				nov += product.price if product.created_at.to_date.month==11
-				dic += product.price if product.created_at.to_date.month==12
-			end
-		end
-
 end
