3684e184e79136a92c18b356977a9b72e80ff041
Omar Valle Silva <dark_earthy@hotmail.com>
2017-03-11 00:24:25 -0300

    Añadidos libros con poco stock en hombre

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,13 @@ module ApplicationHelper
   def numero(number)
     number_with_delimiter(number, :separator => ",", :delimiter => ".")
   end
+
+  def producto(barcode)
+    producto = Product.find_by_barcode(barcode)
+    if producto.nil?
+      producto = Book.find_by_barcode(barcode)
+    end
+    producto
+  end
+
 end
